e33b59ba08e75af43a67c4397c096d840f95fb72
Sam <samsatawat@gmail.com>
2021-07-23 16:14:21 +0700

    update

--- javascript.js
+++ javascript.js
@@ -17,13 +17,13 @@ function animateFrom(elem, direction) {
     direction = direction || 1;
     var x = 0,
         y = direction * 100;
-    if(elem.classList.contains("gs_reveal_fromLeft")) {
-      x = -100;
-      y = 0;
-    } else if (elem.classList.contains("gs_reveal_fromRight")) {
-      x = 100;
-      y = 0;
-    }
+    // if(elem.classList.contains("gs_reveal_fromLeft")) {
+    //   x = -100;
+    //   y = 0;
+    // } else if (elem.classList.contains("gs_reveal_fromRight")) {
+    //   x = 100;
+    //   y = 0;
+    // }
     elem.style.transform = "translate(" + x + "px, " + y + "px)";
     elem.style.opacity = "0";
     gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
